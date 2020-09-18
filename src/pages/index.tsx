cadf6318f03c1231cd193f3ee3c0c5f66075a09b
Knut Sorknes <knut.sorknes@makingwaves.com>
2020-09-18 09:23:33 +0200

    refactor: Update to TS functional component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
 import * as React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+import { token } from "../utils/tokens/index"
+
+export interface IProps {}
+
+const Home: React.FC<IProps> = () => {
+  return (
+    <div
+      style={{
+        padding: token.space[2],
+        backgroundColor: token.color.neutral.TINT_0,
+        color: token.color.neutral.TINT_100,
+      }}>
+      Hello world!
+    </div>
+  )
 }
+
+Home.defaultProps = {}
+
+export default Home
