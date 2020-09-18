40257834a444193eaf06e422ea983cf6597a2662
Knut Sorknes <knut.sorknes@makingwaves.com>
2020-09-18 09:22:25 +0200

    feat: Update color neutral with actual colors

---
+++ src/utils/tokens/color-neutral.ts
@@ -0,0 +1,21 @@
+/**
+ * Generic colors from white to black
+ *
+ * @category Token
+ */
+
+const colorNeutral: object = {
+  TINT_100: "#FFFFFF",
+  TINT_90: "#F2F2F2",
+  TINT_80: "#D9D9D9",
+  TINT_70: "#C0C0C0",
+  TINT_60: "#A6A6A6",
+  TINT_50: "#8D8D8D",
+  TINT_40: "#737373",
+  TINT_30: "#5A5A5A",
+  TINT_20: "#404040",
+  TINT_10: "#262626",
+  TINT_0: "#000000",
+}
+
+export default colorNeutral
