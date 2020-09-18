851c67c4a83d575c086e3332f047ad2bbcba3926
Knut Sorknes <knut.sorknes@makingwaves.com>
2020-09-18 09:22:59 +0200

    feat: Export space token

---
+++ src/utils/tokens/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Collect all tokens in one file.
+ *
+ * @category Token
+ */
+
+import colorNeutral from "./color-neutral"
+import space from "./space"
+
+export const token: any = {
+  color: {
+    neutral: colorNeutral,
+  },
+  space: space,
+}
