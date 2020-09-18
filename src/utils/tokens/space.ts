8e0f6d9d4fbfc4fdb4488d0580bf8f13dc113cf4
Knut Sorknes <knut.sorknes@makingwaves.com>
2020-09-18 09:22:46 +0200

    feat: Add space token

---
+++ src/utils/tokens/space.ts
@@ -0,0 +1,32 @@
+/**
+ * Space tokens are used for padding, margins, and position coordinates.
+ * The design system uses multiples of 4px for all spacing values.
+ *
+ * @category Token
+ */
+
+const space: Array<string> = [
+  "0", // 0px
+  "0.250rem", // 4px
+  "0.500rem", // 8px
+  "0.750rem", // 12px
+  "1.000rem", // 16px
+  "1.250rem", // 20px
+  "1.500rem", // 24px
+  "1.750rem", // 28px
+  "2.000rem", // 32px
+  "2.250rem", // 36px
+  "2.500rem", // 40px
+  "2.750rem", // 44px
+  "3.000rem", // 48px
+  "3.250rem", // 52px
+  "3.500rem", // 56px
+  "3.750rem", // 60px
+  "4.000rem", // 64px
+  "4.250rem", // 68px
+  "4.500rem", // 72px
+  "4.750rem", // 76px
+  "5.000rem", // 80px
+]
+
+export default space
