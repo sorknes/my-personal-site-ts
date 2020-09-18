c2ac1c35eab11abf6d6e1e731fa54e4dd4f2880e
Knut Sorknes <knut.sorknes@makingwaves.com>
2020-09-18 10:14:02 +0200

    feat: Update orange tint

--- src/utils/tokens/color-orange.ts
+++ src/utils/tokens/color-orange.ts
@@ -5,15 +5,15 @@
  */
 
 const colorOrange: object = {
-  TINT_90: "#FAE0D4",
-  TINT_80: "#F5C1A9",
-  TINT_70: "#F1A27D",
-  TINT_60: "#EC8352",
-  TINT_50: "#E76427", // Default
-  TINT_40: "#B9501F",
-  TINT_30: "#8B3C17",
-  TINT_20: "#5C2810",
-  TINT_10: "#2E1408",
+  TINT_90: "#F7D8D1",
+  TINT_80: "#F0B1A3",
+  TINT_70: "#E88B76",
+  TINT_60: "#E16448",
+  TINT_50: "#D93d1A", // Default
+  TINT_40: "#AE3115",
+  TINT_30: "#822510",
+  TINT_20: "#57180A",
+  TINT_10: "#2B0C05",
 }
 
 export default colorOrange
