1b6d7d216e4d38d26584cbf30d2d7257830b8d09
Knut Sorknes <knut.sorknes@makingwaves.com>
2020-09-22 10:45:30 +0200

    feat: Add responsive grid

--- src/components/Layout/Layout.style.ts
+++ src/components/Layout/Layout.style.ts
@@ -1,9 +1,25 @@
 import styled from "styled-components"
 
+import { token } from "../../utils/tokens"
+import { mediaMin } from "../../utils/mixins/mixMediaQuery"
+
 interface IProps {}
 
-const Layout = styled.main`
+const Layout = styled.main<IProps>`
   display: grid;
+  grid-template-columns: minmax(0, 1fr);
+  grid-gap: ${token.space.layout[5]};
+
+  ${mediaMin.S} {
+    grid-template-columns: minmax(0, 1fr) minmax(0, 1fr);
+  }
+
+  ${mediaMin.M} {
+    grid-template-columns: minmax(0, 1fr) minmax(0, 1fr) minmax(0, 1fr) minmax(
+        0,
+        1fr
+      );
+  }
 `
 
 Layout.defaultProps = {}
