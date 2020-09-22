6d718193e4d4851898730a97fdf10da9b8deb675
Knut Sorknes <knut.sorknes@makingwaves.com>
2020-09-22 10:45:08 +0200

    fix: Change breakpoint name

--- src/pages/index.style.ts
+++ src/pages/index.style.ts
@@ -7,16 +7,15 @@ interface IProps {}
 
 const IndexContent = styled.div<IProps>`
   padding: ${token.space.component[2]};
-  margin-bottom: ${token.space.layout[6]};
   color: ${token.color.neutral.TINT_100};
   border-radius: ${token.border.radius[2]};
   background-color: ${token.color.neutral.TINT_0};
 
-  ${mediaMin.VALUE_1} {
+  ${mediaMin.S} {
     background-color: ${token.color.orange.TINT_50};
   }
 
-  ${mediaMin.VALUE_2} {
+  ${mediaMin.M} {
     background-color: ${token.color.blue.TINT_50};
   }
 `
